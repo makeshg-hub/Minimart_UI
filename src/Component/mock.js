00d95bc00216b0dd00c8031e28f2ff8d0fa10b55
makeshg-hub <makeshdhoni7@gmail.com>
2025-04-01 00:08:38 +0530

    product addded

--- src/Component/mock.js
+++ src/Component/mock.js
@@ -1,6 +1,5 @@
 import cinthol from "../assets/pictures/bath/cinthol.jpg";
 import dettol from "../assets/pictures/bath/dettol.jpg";
-import doveShampoo from "../assets/pictures/bath/dove shampoo.jpg";
 import doveSoap from "../assets/pictures/bath/dove.jpg";
 import himalayaFW from "../assets/pictures/bath/face1.jpg";
 import fw2 from "../assets/pictures/bath/face2.jpg";
@@ -11,95 +10,188 @@ import hsShampoo from "../assets/pictures/bath/hs shampoo.jpg";
 import karShampoo from "../assets/pictures/bath/kar shampoo.jpg";
 import pears from "../assets/pictures/bath/pears.jpg";
 import sunsilk from "../assets/pictures/bath/sunsilk.jpg";
-
-
+import sevenUp from "../assets/pictures/beverages/7up.jpg";
+import coffee1 from "../assets/pictures/beverages/coffee1.jpg";
+import coffee2 from "../assets/pictures/beverages/coffee2.jpg";
+import coffee3 from "../assets/pictures/beverages/coffee3.jpg";
+import coke from "../assets/pictures/beverages/coke.jpg";
+import juice1 from "../assets/pictures/beverages/juice es1.jpg";
+import juice2 from "../assets/pictures/beverages/juicemix1.jpg";
+import maza from "../assets/pictures/beverages/maza.jpg";
+import minuteMaid from "../assets/pictures/beverages/minute maid.jpg";
+import pepsi from "../assets/pictures/beverages/pepsi.jpg";
+import soda from "../assets/pictures/beverages/soda.jpg";
+import tea1 from "../assets/pictures/beverages/tea1.jpg";
+import tea2 from "../assets/pictures/beverages/tea2.jpg";
+import tea3 from "../assets/pictures/beverages/tea3.jpg";
+import tea4 from "../assets/pictures/beverages/tea4.jpg";
+import atta from "../assets/pictures/essential/atta1.jpg";
+import chillipowder from "../assets/pictures/essential/chilli powder.jpg";
+import maida from "../assets/pictures/essential/maida.jpg";
+import moongDal from "../assets/pictures/essential/moong dal.jpg";
+import oil1 from "../assets/pictures/essential/oil1.jpg";
+import oil2 from "../assets/pictures/essential/oil2.jpg";
+import oil3 from "../assets/pictures/essential/oil3.jpg";
+import oridDal from "../assets/pictures/essential/orid dal.jpg";
+import rava from "../assets/pictures/essential/rava.jpg";
+import rice1 from "../assets/pictures/essential/rice1.jpg";
+import rice2 from "../assets/pictures/essential/rice2.jpg";
+import salt1 from "../assets/pictures/essential/salt1.jpg";
+import salt2 from "../assets/pictures/essential/salt2.jpg";
+import sugar1 from "../assets/pictures/essential/sugar1.jpg";
+import sugar2 from "../assets/pictures/essential/sugar2.jpg";
+import toorDal from "../assets/pictures/essential/toor dal.jpg";
+import axe from "../assets/pictures/grooming/axe.jpg";
+import axe1 from "../assets/pictures/grooming/axe1.jpg";
+import axe2 from "../assets/pictures/grooming/axe2.jpg";
+import flora from "../assets/pictures/grooming/flora.webp";
+import fogg from "../assets/pictures/grooming/fogg black.webp";
+import hairOil1 from "../assets/pictures/grooming/hair oil1.jpg";
+import hairOil2 from "../assets/pictures/grooming/hair oil2.jpg";
+import razer1 from "../assets/pictures/grooming/razer1.jpg";
+import sunscreen from "../assets/pictures/grooming/ss1.jpg";
+import nivea from "../assets/pictures/grooming/nivea.jpg";
+import neud from "../assets/pictures/grooming/neud.avif";
+import noor from "../assets/pictures/grooming/noor.jpg";
+import apple from "../assets/pictures/fruits&vegs/apple.jpg";
+import banana from "../assets/pictures/fruits&vegs/banana.jpg";
+import tomato from "../assets/pictures/fruits&vegs/tomato.jpg";
+import onion from "../assets/pictures/fruits&vegs/onion.jpg";
+import potato from "../assets/pictures/fruits&vegs/potato.jpg";
+import mango from "../assets/pictures/fruits&vegs/mango.jpg";
+import pomegranate from "../assets/pictures/fruits&vegs/pomegranate.jpg";
+import carrot from "../assets/pictures/fruits&vegs/carrot.jpg";
+import cabbage from "../assets/pictures/fruits&vegs/cabbage.jpg";
+import cauliflower from "../assets/pictures/fruits&vegs/cauliflower.jpg";
+import milk  from "../assets/pictures/dairy/milk.jpg";
+import egg  from "../assets/pictures/dairy/egg.jpg";
+import cheese  from "../assets/pictures/dairy/cheese.jpg";
+import butter  from "../assets/pictures/dairy/butter.jpg";
+import curd  from "../assets/pictures/dairy/curd.jpg";
+import ghee  from "../assets/pictures/dairy/ghee.jpg";
+import paneer  from "../assets/pictures/dairy/paneer.jpg";
+import chicken from "../assets/pictures/meat/chicken.jpg";
+import mutton from "../assets/pictures/meat/mutton.jpg";
+import fish from "../assets/pictures/meat/fish.jpg";
+import prawn from "../assets/pictures/meat/prawn.jpg";
+import crab from "../assets/pictures/meat/crab.jpg";
+import bread from "../assets/pictures/bakery/bread.jpg";
+import whiteBread from "../assets/pictures/bakery/wheat bread.jpg";
+import muffin from "../assets/pictures/bakery/muffin.jpg";
+import croissant from "../assets/pictures/bakery/croissant.jpg";
+import pav from "../assets/pictures/bakery/pav.jpg";
+import maggi from "../assets/pictures/packed food/maggi1.jpg";
+import paniPuri from "../assets/pictures/packed food/panipoori.jpg";
+import pasta from "../assets/pictures/packed food/pasta masala.jpg";
+import mtrJamun from "../assets/pictures/packed food/mtr.jpg";
+import yippee from "../assets/pictures/packed food/yippee.jpg";
+import chips from "../assets/pictures/snacks/chips.jpg";
+import namkeen from "../assets/pictures/snacks/namkeen.jpg";
+import chocolate from "../assets/pictures/snacks/chocolate.jpg";
+import biscuits from "../assets/pictures/snacks/biscuits.jpg";
+import popcorn from "../assets/pictures/snacks/popcorn.jpg";
+import frozenCutlet from "../assets/pictures/snacks/cutlet.jpg";
+import frozenFries from "../assets/pictures/snacks/fries.jpg";
+import frozenChickenNuggets from "../assets/pictures/snacks/chicken nuggets.jpg";
+import frozenParatha from "../assets/pictures/snacks/paratha.jpg";
+import icecream from "../assets/pictures/snacks/ice cream.jpg";
+import menWatch from "../assets/pictures/fashion/accessories/watch1.jpg";
+import womenWatch from "../assets/pictures/fashion/accessories/watch2.jpg";
+import nikeBag from "../assets/pictures/fashion/bag/bag.jpg";
+import ladiesBag from "../assets/pictures/fashion/bag/bag1.jpg";
+import kidsBag from "../assets/pictures/fashion/bag/kids bag.jpg";
+import adidas from "../assets/pictures/fashion/footwear/adidas.jpg";
+import yellowShoe from "../assets/pictures/fashion/footwear/L shoe1.webp";
+import menShirt from "../assets/pictures/fashion/shirt/shirt1.jpg";
+import womenTop from "../assets/pictures/fashion/shirt/top1.jpg";
+import tshirt from "../assets/pictures/fashion/shirt/shirt3.jpg";
 
 export const groceryData = [
   {
     category: "Fruits & Vegetables",
     products: [
-      { name: "Apple (1kg)", price: 180, id: 1, img: cinthol },
-      { name: "Banana (Dozen)", price: 60, id: 2 },
-      { name: "Tomato (1kg)", price: 40, id: 3 },
-      { name: "Potato (1kg)", price: 30, id: 4 },
-      { name: "Onion (1kg)", price: 35, id: 5 },
-      { name: "Carrot (1kg)", price: 50, id: 6 },
-      { name: "Cabbage (1 piece)", price: 40, id: 7 },
-      { name: "Cauliflower (1 piece)", price: 50, id: 8 },
-      { name: "Mango (1kg)", price: 120, id: 9 },
-      { name: "Pomegranate (1kg)", price: 200, id: 10 }
+      { name: "Apple (1kg)", price: 180, id: 1,img:apple },
+      { name: "Banana (Dozen)", price: 60, id: 2,img:banana },
+      { name: "Tomato (1kg)", price: 40, id: 3,img:tomato },
+      { name: "Potato (1kg)", price: 30, id: 4,img:potato },
+      { name: "Onion (1kg)", price: 35, id: 5 ,img:onion},
+      { name: "Carrot (1kg)", price: 50, id: 6 ,img:carrot},
+      { name: "Cabbage (1 piece)", price: 40, id: 7,img:cabbage},
+      { name: "Cauliflower (1 piece)", price: 50, id: 8 ,img:cauliflower},
+      { name: "Mango (1kg)", price: 120, id: 9 ,img:mango},
+      { name: "Pomegranate (1kg)", price: 200, id: 10,img:pomegranate }
     ]
   },
   {
     category: "Dairy & Eggs",
     products: [
-      { name: "Milk (1L)", price: 60, id: 11 },
-      { name: "Eggs (12 pack)", price: 85, id: 12 },
-      { name: "Butter (500g)", price: 270, id: 13 },
-      { name: "Cheese (200g)", price: 130, id: 14 },
-      { name: "Paneer (500g)", price: 180, id: 15 },
-      { name: "Curd (500g)", price: 45, id: 16 },
-      { name: "Ghee (1L)", price: 550, id: 17 }
+      { name: "Milk (1L)", price: 60, id: 11 ,img:milk},
+      { name: "Eggs (12 pack)", price: 85, id: 12 ,img:egg},
+      { name: "Butter (500g)", price: 270, id: 13 ,img:butter},
+      { name: "Cheese (200g)", price: 130, id: 14 ,img:cheese},
+      { name: "Paneer (500g)", price: 180, id: 15 ,img:paneer},
+      { name: "Curd (500g)", price: 45, id: 16 ,img:curd},
+      { name: "Ghee (1L)", price: 550, id: 17,img:ghee }
     ]
   },
   {
     category: "Meat & Seafood",
     products: [
-      { name: "Chicken (1kg)", price: 250, id: 18 },
-      { name: "Mutton (1kg)", price: 750, id: 19 },
-      { name: "Fish (1kg)", price: 400, id: 20 },
-      { name: "Prawns (500g)", price: 450, id: 21 },
-      { name: "Eggs (30 pack)", price: 200, id: 22 }
+      { name: "Chicken (1kg)", price: 250, id: 18 ,img:chicken},
+      { name: "Mutton (1kg)", price: 750, id: 19 ,img:mutton},
+      { name: "Fish (1kg)", price: 400, id: 20,img:fish },
+      { name: "Prawns (500g)", price: 450, id: 21,img:prawn },
+      { name: "Crab", price: 200, id: 22 ,img:crab}
     ]
   },
   {
     category: "Bakery",
     products: [
-      { name: "Bread (400g)", price: 50, id: 23 },
-      { name: "Whole Wheat Bread (400g)", price: 60, id: 24 },
-      { name: "Croissant (Pack of 2)", price: 120, id: 25 },
-      { name: "Pav (6 pieces)", price: 40, id: 26 },
-      { name: "Muffin (Pack of 2)", price: 100, id: 27 }
+      { name: "Bread (400g)", price: 50, id: 23,img:bread },
+      { name: "Whole Wheat Bread (400g)", price: 60, id:24,img:whiteBread },
+      { name: "Croissant (Pack of 2)", price: 120, id: 25 ,img:croissant},
+      { name: "Pav (6 pieces)", price: 40, id: 26 ,img:pav},
+      { name: "Muffin (Pack of 2)", price: 100, id: 27 ,img:muffin}
     ]
   },
   {
-    category: "Beverages",
+    category: "packed Foods",
     products: [
-      { name: "Tea (500g)", price: 260, id: 28 },
-      { name: "Coffee (200g)", price: 400, id: 29 },
-      { name: "Orange Juice (1L)", price: 120, id: 30 },
-      { name: "Soft Drink (750ml)", price: 40, id: 31 },
-      { name: "Coconut Water (1L)", price: 90, id: 32 }
+      { name: "Maggi", price: 18, id: 28 ,img:maggi},
+      { name: "Pani Puri", price: 120, id: 29 ,img:paniPuri},
+      { name: "Pasta", price: 75, id: 30 ,img:pasta},
+      { name: "MTR Gulab Jamun", price: 139, id: 31 ,img:mtrJamun},
+      { name: "Yippee", price: 65, id: 32 ,img:yippee}
     ]
   },
   {
     category: "Snacks & Frozen",
     products: [
-      { name: "Chips (200g)", price: 50, id: 33 },
-      { name: "Namkeen (500g)", price: 120, id: 34 },
-      { name: "Chocolate (100g)", price: 150, id: 35 },
-      { name: "Biscuits (Pack of 5)", price: 80, id: 36 },
-      { name: "Popcorn (100g)", price: 70, id: 37 },
-      { name: "Frozen Peas (1kg)", price: 150, id: 38 },
-      { name: "Frozen French Fries (1kg)", price: 180, id: 39 },
-      { name: "Frozen Paratha (Pack of 5)", price: 120, id: 40 },
-      { name: "Frozen Chicken Nuggets (500g)", price: 220, id: 41 },
-      { name: "Ice Cream (500ml)", price: 180, id: 42 }
+      { name: "Chips (200g)", price: 50, id: 33 ,img:chips},
+      { name: "Namkeen (500g)", price: 120, id: 34 ,img:namkeen},
+      { name: "Chocolate (100g)", price: 150, id: 35,img:chocolate },
+      { name: "Biscuits (Pack of 5)", price: 80, id: 36,img:biscuits },
+      { name: "Popcorn (100g)", price: 70, id: 37 ,img:popcorn},
+      { name: "Frozen Cutlet (1kg)", price: 220, id: 38 ,img:frozenCutlet},
+      { name: "Frozen French Fries (1kg)", price: 270, id: 39 ,img:frozenFries},
+      { name: "Frozen Paratha (Pack of 5)", price: 120, id: 40 ,img:frozenParatha},
+      { name: "Frozen Chicken Nuggets (500g)", price: 325, id: 41,img:frozenChickenNuggets },
+      { name: "Ice Cream (500ml)", price: 180, id: 42 ,img:icecream}
     ]
   },
   {
-    category: "Pantry Staples",
+    category: "Fashion",
     products: [
-      { name: "Rice (5kg)", price: 350, id: 43 },
-      { name: "Wheat Flour (5kg)", price: 220, id: 44 },
-      { name: "Pulses (1kg)", price: 140, id: 45 },
-      { name: "Sugar (1kg)", price: 45, id: 46 },
-      { name: "Salt (1kg)", price: 20, id: 47 },
-      { name: "Cooking Oil (1L)", price: 180, id: 48 },
-      { name: "Soy Sauce (250ml)", price: 60, id: 49 },
-      { name: "Ketchup (500g)", price: 80, id: 50 },
-      { name: "Mustard Sauce (250ml)", price: 75, id: 51 },
-      { name: "Mayonnaise (500g)", price: 120, id: 52 }
+      { name: "watch Men", price: 2199, id: 43,img:menWatch },
+      { name: "Rose Gold Watch", price: 1899, id: 44 ,img:womenWatch},
+      { name: "Nike Bag", price: 899, id: 45 ,img:nikeBag},
+      { name: "Ladies Bag", price: 1099, id: 46 ,img:ladiesBag},
+      { name: "Kids Bag", price: 680, id: 47 ,img:kidsBag},
+      { name: "Adidas Shoe", price: 3499, id: 48 ,img:adidas},
+      { name: "Yellow Flat Heel", price: 979, id: 49 ,img:yellowShoe},
+      { name: "Men Shirt", price: 675, id: 50 ,img:menShirt},
+      { name: "Women Top", price: 520, id: 51 ,img:womenTop},
+      { name: "Men T-Shirt", price: 120, id: 52 ,img:tshirt}
     ]
   },
   {
@@ -107,7 +199,7 @@ export const groceryData = [
     products:[
         {name:"Cinthol Soap", price:60,id:53, img:cinthol},
         {name:"Dettol Soap",price:50,id:54, img:dettol},
-        {name:"Dove Soap",price:65,id:55, img:doveShampoo},
+        {name:"Dove Soap",price:65,id:55, img:doveSoap},
         {name:"Himalaya Facewash",price:120,id:56 ,img:himalayaFW},
         {name:"Cetaphil Facewash",price:190,id:57, img:fw2},
         {name:"Clean&clear Facewash",price:235,id:58,img:fw3},
@@ -122,60 +214,60 @@ export const groceryData = [
   {
     category:"Beverages",
     products:[
-      {name:"7up 600ml",price:48,id:65},
-      {name:"Bru 10rs",price:10,id:66},
-      {name:"Sunrise 20rs",price:20,id:67},
-      {name:"Narasu's 50g",price:65,id:68},
-      {name:"Soda",price:25,id:69},      
-      {name:"Coke",price:47,id:70},
-      {name:"Mala's Strawberry mix",price:189,id:71},
-      {name:"Orange Mix",price:35,id:72},
-      {name:"Maza 600ml",price:45,id:73},
-      {name:"Minute-Maid",price:25,id:74},
-      {name:"Red Label",price:179,id:75},
-      {name:"Tetley Green Tea",price:219,id:76},
-      {name:"Kanan Devan ",price:120,id:77},
-      {name:"Lipton Green Tea",price:140,id:78},
-      {name:"7up",price:48,id:79},
+      {name:"7up 600ml",price:48,id:65 ,img:sevenUp},
+      {name:"Bru 10rs",price:10,id:66, img:coffee1},
+      {name:"Sunrise 20rs",price:20,id:67 ,img:coffee2},
+      {name:"Narasu's 50g",price:65,id:68,img:coffee3},
+      {name:"Soda",price:25,id:69, img:soda},      
+      {name:"Coke",price:47,id:70,img:coke},
+      {name:"Mala's Strawberry mix",price:189,id:71, img:juice1},
+      {name:"Orange Mix",price:35,id:72,img:juice2},
+      {name:"Maza 600ml",price:45,id:73,img:maza},
+      {name:"Minute-Maid",price:25,id:74,img:minuteMaid},
+      {name:"Red Label",price:179,id:75,img:tea1},
+      {name:"Tetley Green Tea",price:219,id:76,img:tea2},
+      {name:"Kanan Devan ",price:120,id:77,img:tea3},
+      {name:"Lipton Green Tea",price:140,id:78,img:tea4},
+      {name:"Pepsi 600ml",price:48,id:79,img:pepsi},
     ]
   },
   {
     category:"Essential",
     products:[
-      {name:"Fortune Atta",price:65,id:80,},
-      {name:"Eastern chilli Powder",price:30,id:81},
-      {name:"Maida 500g",price:30,id:82},
-      {name:"Toor Dhall",price:160,id:83},
-      {name:"Moong Dhall",price:135,id:84},
-      {name:"Orid Dhall",price:175,id:85},
-      {name:"sugar 1kg",price:52,id:86},
-      {name:"Parrry's Sugar",price:65,id:87},
-      {name:"Fortune oil 1L",price:152,id:88},
-      {name:"Saffola Gold 1L",price:183,id:89},
-      {name:"Ground Nut oil",price:220,id:90},
-      {name:"Bamati rice 5kg",price:720,id:91},
-      {name:"Boiled Rice 26kg",price:1660,id:92},
-      {name:"Naga Rava 500g",price:48,id:93},
-      {name:"Pink Salt",price:90,id:94},
-      {name:"Rock Salt",price:38,id:95},
+      {name:"Fortune Atta",price:65,id:80,img:atta},
+      {name:"Eastern chilli Powder",price:30,id:81,img:chillipowder},
+      {name:"Maida 500g",price:30,id:82,img:maida},
+      {name:"Toor Dhall",price:160,id:83,img:toorDal},
+      {name:"Moong Dhall",price:135,id:84,img:moongDal},
+      {name:"Orid Dhall",price:175,id:85,img:oridDal},
+      {name:"sugar 1kg",price:52,id:86,img:sugar1},
+      {name:"Parrry's Sugar",price:65,id:87,img:sugar2},
+      {name:"Fortune oil 1L",price:152,id:88,img:oil1},
+      {name:"Saffola Gold 1L",price:183,id:89,img:oil2},
+      {name:"Ground Nut oil",price:220,id:90,img:oil3},
+      {name:"Bamati rice 5kg",price:720,id:91,img:rice1},
+      {name:"Boiled Rice 26kg",price:1660,id:92,img:rice2},
+      {name:"Naga Rava 500g",price:48,id:93,img:rava},
+      {name:"Pink Salt",price:90,id:94,img:salt1},
+      {name:"Rock Salt",price:38,id:95,img:salt2},
 
     ]
   },
   {
     category:"Grooming",
     products:[
-      {name:"Axe Body Spray", price:180,id:96},
-      {name:"Axe Signature",price:215,id:97},
-      {name:"Axe Pocket",price:40,id:98},
-      {name:"Flora Women",price:249,id:99},
-      {name:"Fogg Black",price:299,id:100},
-      {name:"Neud Perfume",price:199,id:101},
-      {name:"Noor Perfume",price:320,id:102},
-      {name:"Parachute Hair Oil",price:99,id:103},
-      {name:"Dabur Amla oil",price:135,id:104},
-      {name:"Gillette Razor",price:89,id:105},
-      {name:"Venus Razor",price:119,id:106},
-      {name:"Nivea Soft",price:265,id:107}
+      {name:"Axe Body Spray", price:180,id:96,img:axe},
+      {name:"Axe Signature",price:215,id:97,img:axe1},
+      {name:"Axe Pocket",price:40,id:98,img:axe2},
+      {name:"Flora Women",price:249,id:99,img:flora},
+      {name:"Fogg Black",price:299,id:100,img:fogg},
+      {name:"Neud Perfume",price:199,id:101,img:neud},
+      {name:"Noor Perfume",price:320,id:102,img:noor},
+      {name:"Parachute Hair Oil",price:99,id:103,img:hairOil1},
+      {name:"Dabur Amla oil",price:135,id:104,img:hairOil2},
+      {name:"Gillette Razor",price:89,id:105,img:razer1},
+      {name:"Sunscreen",price:119,id:106,img:sunscreen},
+      {name:"Nivea Soft",price:265,id:107,img:nivea}
 
     ]
   }
